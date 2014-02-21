d528fd11526722320f77d47ee5e58dd6385f34ce
Clinton Weiss <cvweiss@gmail.com>
2014-02-21 17:26:37 +0000

    Need this class too

---
+++ classes/Email.php
@@ -0,0 +1,8 @@
+﻿<?php
+class Email
+{
+	public static function create($email, $subject, $body) {
+		Db::execute("insert into skq_emails (recipient, subject, content) values (:e, :s, :c)",
+			array(":e" => $email, ":s" => $subject, ":c" => $body));
+	}
+}
