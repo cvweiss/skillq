a8c7cc5cbe68f90e38da185273e0b64546b4281f
Clinton Weiss <cvweiss@gmail.com>
2014-02-23 00:10:04 +0000

    Proper Db function

--- classes/Registration.php
+++ classes/Registration.php
@@ -13,7 +13,7 @@ class Registration
 		$check = Db::queryField("SELECT count(*) count FROM skq_users WHERE email = :email OR username = :username", "count", array(":email" => $email, ":username" => $username), 0);
 		if ($check == 0) {
 			$hashedpassword = Password::genPassword($password);
-			Db::query("INSERT INTO skq_users (username, password, email) VALUES (:username, :password, :email)", array(":username" => $username, ":password" => $hashedpassword, ":email" => $email));
+			Db::execute("INSERT INTO skq_users (username, password, email) VALUES (:username, :password, :email)", array(":username" => $username, ":password" => $hashedpassword, ":email" => $email));
 			$subject = "$siteName Registration";
 			$message = "Thank you, $username, for registering at $siteName";
 			Email::create($email, $subject, $message);
