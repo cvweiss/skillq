0e2adf8808082c469c052f004f55f5d6231f120d
Clinton V. Weiss <cvweiss@gmail.com>
2018-03-12 15:06:57 +0000

    removes characters that have no valid refreshToken scopes

---
+++ util/purge_chars.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once "../init.php";
+
+$result = Db::query("select * from (select characterID, count(*) count from skq_scopes group by 1) as foo where count = 1");
+$tables = ['skq_character_assets', 'skq_character_certs', 'skq_character_implants', 'skq_character_info', 'skq_character_queue', 'skq_character_shares', 'skq_character_skills', 'skq_character_training', 'skq_character_wallet', 'skq_scopes'];
+foreach ($result as $row) {
+	$charID = $row['characterID'];
+	foreach ($tables as $table) {
+		Db::execute("delete from $table where characterID = :charID", [':charID' => $charID]);
+	}
+	Db::execute("delete from skq_character_associations where char1 = :charID or char2 = :charID", [':charID' => $charID]);
+}
