cf04b75cc47e5b8815bcbfe9693059b6f8b9fa8e
Clinton V. Weiss <cvweiss@gmail.com>
2018-05-22 22:03:19 +0000

    prevent ambiguous error

--- cron/purge_chars.php
+++ cron/purge_chars.php
@@ -5,7 +5,7 @@ require_once "../init.php";
 if (date('i') != 0) exit();
 
 $result = Db::query("select * from (select characterID, count(*) count from skq_scopes group by 1) as foo where count = 1");
-$tables = ['skq_character_assets', 'skq_character_certs', 'skq_character_implants', 'skq_character_info', 'skq_character_queue', 'skq_character_shares', 'skq_character_skills', 'skq_character_training', 'skq_character_wallet', 'skq_scopes', 'skq_users', 'skq_users_config'];
+$tables = ['skq_character_assets', 'skq_character_certs', 'skq_character_implants', 'skq_character_info', 'skq_character_queue', 'skq_character_shares', 'skq_character_skills', 'skq_character_training', 'skq_character_wallet', 'skq_scopes', 'skq_users', 'skq_users_config, skq_character_tracking'];
 foreach ($result as $row) {
 	$charID = $row['characterID'];
 	Util::out("Cleaning up $charID");
@@ -16,7 +16,7 @@ foreach ($result as $row) {
 }
 
 foreach ($tables as $table) {
-	$result = Db::query("select distinct characterID from $table where characterID not in (select distinct characterID from skq_scopes)");
+	$result = Db::query("select distinct a.characterID from a.$table where a.haracterID not in (select distinct b.characterID from b.skq_scopes)");
 	if (sizeof($result)) {
 		foreach ($result as $row) {
 			$charID = $row['characterID'];
