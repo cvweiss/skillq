0cee5805244e21d6bccc19221cc2258d5404e98e
Clinton V. Weiss <cvweiss@gmail.com>
2018-05-29 11:55:15 +0000

    fixed a couple of silly issues

--- cron/purge_chars.php
+++ cron/purge_chars.php
@@ -5,7 +5,7 @@ require_once "../init.php";
 if (date('i') != 0) exit();
 
 $result = Db::query("select * from (select characterID, count(*) count from skq_scopes group by 1) as foo where count = 1");
-$tables = ['skq_character_assets', 'skq_character_certs', 'skq_character_implants', 'skq_character_info', 'skq_character_queue', 'skq_character_shares', 'skq_character_skills', 'skq_character_training', 'skq_character_wallet', 'skq_scopes', 'skq_users', 'skq_users_config, skq_character_tracking'];
+$tables = ['skq_character_assets', 'skq_character_certs', 'skq_character_implants', 'skq_character_info', 'skq_character_queue', 'skq_character_shares', 'skq_character_skills', 'skq_character_training', 'skq_character_wallet', 'skq_scopes', 'skq_users', 'skq_users_config', 'skq_character_tracking'];
 foreach ($result as $row) {
 	$charID = $row['characterID'];
 	Util::out("Cleaning up $charID");
@@ -16,11 +16,12 @@ foreach ($result as $row) {
 }
 
 foreach ($tables as $table) {
-	$result = Db::query("select distinct a.characterID from a.$table where a.haracterID not in (select distinct b.characterID from b.skq_scopes)");
+	$result = Db::query("select distinct a.characterID from $table a where a.characterID not in (select distinct b.characterID from skq_scopes b)");
 	if (sizeof($result)) {
 		foreach ($result as $row) {
 			$charID = $row['characterID'];
 			Util::out("Removing $charID from $table");
+echo "delete from $table where characterID = :charID $charID\n";
 			Db::execute("delete from $table where characterID = :charID", [':charID' => $charID]);
 		}
 	}
