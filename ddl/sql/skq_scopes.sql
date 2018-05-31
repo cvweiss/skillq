1ee5ffce68a0c1da4f8567cdf3efb48e6fbd7436
Clinton V. Weiss <cvweiss@gmail.com>
2018-05-31 06:41:04 +0000

    ddl update

--- ddl/sql/skq_scopes.sql
+++ ddl/sql/skq_scopes.sql
@@ -7,6 +7,7 @@ CREATE TABLE `skq_scopes` (
   `lastChecked` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `lastErrorCode` decimal(4,0) NOT NULL DEFAULT '0',
   `errorCount` decimal(2,0) NOT NULL DEFAULT '0',
+  `lastSsoChecked` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   UNIQUE KEY `char_scope` (`characterID`,`scope`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
