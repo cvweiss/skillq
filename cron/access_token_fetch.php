aaedac34930702eeb8b117b1bc5ad3ddfed9ea8b
Clinton V. Weiss <cvweiss@gmail.com>
2018-06-02 03:34:20 +0000

    slow it down

--- cron/access_token_fetch.php
+++ cron/access_token_fetch.php
@@ -4,7 +4,7 @@ require_once "../init.php";
 
 use zkillboard\crestsso\CrestSSO;
 
-$guzzler = Util::getGuzzler(10);
+$guzzler = Util::getGuzzler(5);
 
 global $clientID, $secretKey, $callbackURL, $scopes;
 
