31d8bfcc8425460b2d500515ba9fe74794270f53
Clinton Weiss <cvweiss@gmail.com>
2019-03-14 22:25:05 +0000

    setup change

--- install/sde_update/sde_update.php
+++ install/sde_update/sde_update.php
@@ -2,14 +2,16 @@
 
 require_once "../../init.php";
 
+$sdeDb = $argv[1];
 $dbUser = $argv[2];
 $dbPassword = $argv[3];
+
+echo "db: $sdeDb user $dbUser pass: '$dbPassword'\n";
 
 $tables = array("dgmAttributeCategories", "dgmAttributeTypes", "dgmTypeAttributes", "invGroups", "invTypes");
 
 foreach ($tables as $table)
 {
-	$sdeDb = $argv[1];
 	$ourTable = $table;
 	if ($ourTable == "mapRegions") $ourTable = "regions";
 	if ($ourTable == "mapSolarSystems") $ourTable = "systems";
